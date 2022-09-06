4c6107fe94a331415f06f45053891919af5fc07e
hafsadahir <hafsa.dahir@student.moringaschool.com>
2022-09-06 14:14:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ function isPalindrome(word) {
 /*
   Add written explanation of your solution here
 */
+function isPalindrome(word) {
+  let wordArray = word.split("");
+  let reversedWord = wordArray.reverse().join("");
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
